8fe934e8d6fe19d68f3794329ea537a4c3eedc4b
xzp <xingzepeng@geo-compass.com>
2022-06-01 17:43:59 +0800

    feat: resolve-plugin

--- 12.rollup-plugin/plugins/rollup-plugin-resolveFileUrl.js
+++ 12.rollup-plugin/plugins/rollup-plugin-resolveFileUrl.js
@@ -1,5 +1,22 @@
 const resolveFilePlugin = (options = {}) => {
+  return {
+    name: "resolveFileUrl",
+    resolveId(source) {
+      if (source === "logger") {
+        return source;
+      }
+    },
+    load(importee) {
+      if (importee === "logger") {
+        //向输出目录里生成一个新的文件，文件名叫logger.js 文件内容叫  'export default "logger"'
+        let referenceId = this.emitFile({
+          type: "asset",
+          source: 'export default "logger"',
+          fileName: "logger.js",
+        });
+      }
+    },
+  };
+};
 
-}
-
-export default resolveFilePlugin
+export default resolveFilePlugin;
--- vite522/lib/plugins/resolve.js
+++ vite522/lib/plugins/resolve.js
@@ -1,20 +1,44 @@
-const fs = require('fs-extra')
-const path = require('path')
-const resolve = require('resolve')
-
+const fs = require("fs-extra");
+const path = require("path");
+const resolve = require("resolve");
 
 // vite插件
 function resolvePlugin(config) {
-    return{
-        name: 'vite:resolve',
-        resolveId(importee, importer){
-            if (importee.startsWith('/')) { //说明这个相对于项目根目录的绝对路径 /src/main.js
-                return {id: path.resolve(config.root, importee.slice(1))} //项目根目录 /src/main.js
-            }
-            //如果已经是硬盘上的绝对路径了可以直接返回
-            if (path.isAbsolute(importee)) {
-                return {id: importee}
-            }
-        }
-    }
+  return {
+    name: "vite:resolve",
+    resolveId(importee, importer) {
+      if (importee.startsWith("/")) {
+        //说明这个相对于项目根目录的绝对路径 /src/main.js
+        return { id: path.resolve(config.root, importee.slice(1)) }; //项目根目录 /src/main.js
+      }
+      //如果已经是硬盘上的绝对路径了可以直接返回
+      if (path.isAbsolute(importee)) {
+        return { id: importee };
+      }
+      //说明这是一个相对路径
+      if (importee.startsWith(".")) {
+        const baseDir = path.dirname(importer);
+        const fsPath = path.resolve(baseDir, importee);
+        return { id: fsPath };
+      }
+      //其它就是第三方模块
+      let res = tryNodeResolve(importee, importer, config);
+      if (res) {
+        return res;
+      }
+    },
+  };
 }
+
+function tryNodeResolve(importee, importer, config) {
+  const pkgPath = resolve.sync(`${importee}/package.json`, {
+    basedir: config.root,
+  });
+  const pkgDir = path.dirname(pkgPath);
+  const pkg = JSON.parse(fs.readFileSync(pkgPath, "utf-8"));
+  const entryPoint = pkg.module; //包描述文件中的module属性执行此模块的es6版本代码
+  const entryPointPath = path.join(pkgDir, entryPoint);
+  return { id: entryPointPath };
+}
+
+module.exports = resolvePlugin;
